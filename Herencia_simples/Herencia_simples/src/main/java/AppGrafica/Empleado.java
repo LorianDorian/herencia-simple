d1de77b9ccee9be15fd0ad315b585093479dfe0f
André Alderete <67977289+LorianDorian@users.noreply.github.com>
2023-04-23 01:04:28 -0600

    Add files via upload

---
+++ Herencia_simples/Herencia_simples/src/main/java/AppGrafica/Empleado.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AppGrafica;
+
+/**
+ *
+ * @author docec
+ */
+public class Empleado extends Persona {
+    private String _puesto; 
+    private int _salario;
+}
---
+++ Herencia_simples/Herencia_simples/src/main/java/AppGrafica/Persona.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package AppGrafica;
+
+/**
+ *
+ * @author docec
+ */
+public class Persona {
+    
+    private String _nombre; 
+    private int _edad; 
+    private char _sexo; 
+}
